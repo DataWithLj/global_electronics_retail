485cfdd8eb32355b4f6c4ff171a7d08003413ac7
DataWithLj <ldnjpalma@gmail.com>
2026-08-26 21:42:36 +0800

    Delete SQL directory

--- SQL/delivery_storekey_check.sql
+++
@@ -1,5 +0,0 @@
--- Checking non-null delivery date counts for online (StoreKey 0) vs physical stores
-SELECT
-  COUNTIF(StoreKey = 0 AND `Delivery Date` IS NOT NULL) AS online_deldate,
-  COUNTIF(StoreKey != 0 AND `Delivery Date` IS NOT NULL) AS physical_deldate
-FROM `casestudyanalysis.electronics_retail.sales`
